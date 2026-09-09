0f796605ba99ff5e6a07f7508c0f358d25389bdb
theOguz16 <98805835+theOguz16@users.noreply.github.com>
2026-09-09 13:56:21 +0300

    P0.2: add integrations public entrypoint

---
+++ packages/integrations/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./coding-executor.js";
+export * from "./local-openai-compatible-model-client.js";
+export * from "./runpod-openai-compatible-model-client.js";
+export * from "./provider-execution-error.js";
